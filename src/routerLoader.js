86eb8a48035bdcd0f4f2171b8078c0f40e286324
jlribeirojr <jlribeirojr@gmail.com>
2023-08-11 23:12:53 -0300

    insert Router

---
+++ src/routerLoader.js
@@ -0,0 +1,33 @@
+import { dir } from 'console';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+export const routerLoader = (app) => {
+    const modulesPath = path.join(__dirname, 'modules');
+
+    fs.readdirSync(modulesPath).forEach(async (dir) => {
+        const modulePath = path.join(modulesPath, dir)
+
+
+        if (fs.statSync(modulePath).isDirectory()) {
+            const controllerPath = path.join(modulesPath, `${dir}.controller.js`)
+
+
+            if (fs.existsSync(controllerPath)) {
+                const controller = await import(controllerPath);
+
+                if (controller.default && typeof controller.default === 'function') {
+                    app.use(controller.default);
+                }
+            }
+        }
+    })
+
+
+
+}
