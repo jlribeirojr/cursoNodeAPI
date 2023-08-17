10b6bd89008a39c538dd7b3eddb8a69734980afe
jlribeirojr <jlribeirojr@gmail.com>
2023-08-16 22:54:46 -0300

    insert DTO

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { UserModel } from './user.model';
+import { UserInsertDTO } from 'src/user/dtos/user-isert.dto';
 const prisma = new PrismaClient();
 
-export const getUsers = async () => {
+export const getUsers = async (): Promise<UserModel[]> => {
   return prisma.user.findMany();
 };
-export const creatUser = async (body) => {
+export const creatUser = async (body: UserInsertDTO): Promise<UserModel> => {
   return prisma.user.create({
     data: body,
   });
