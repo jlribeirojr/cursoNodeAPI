7e2324b48cce6bbff44b928c7b4d2f93230e5940
jlribeirojr <jlribeirojr@gmail.com>
2023-08-19 19:38:41 -0300

    Create import to export to extension for typescript automatic

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { UserModel } from './user.model';
 import { UserInsertDTO } from 'src/user/dtos/user-isert.dto';
+import { NotFoundException } from '@exceptions/not-found-exception';
 const prisma = new PrismaClient();
 
 export const getUsers = async (): Promise<UserModel[]> => {
+  const users = await prisma.user.findMany();
   return prisma.user.findMany();
 };
 export const creatUser = async (body: UserInsertDTO): Promise<UserModel> => {
