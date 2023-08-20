bc1285afcd4ded40538354ce68d2501bfb5d155c
jlribeirojr <jlribeirojr@gmail.com>
2023-08-20 12:55:52 -0300

    again insert folder util and passoword bcrypt

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,8 +1,8 @@
 import { hash, compare } from 'bcrypt';
 export const createPasswordHashed = async (password: string): Promise<string> => {
-    const saltRounds = 10;
+  const saltRounds = 10;
 
-    return hash(password, saltRounds);
+  return hash(password, saltRounds);
 };
 
 export const validatePassword = async (
